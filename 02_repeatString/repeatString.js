f17fca2628d310e4afe58445b33587a7ef341e9f
Dmitry Shamko <proffery@gmail.com>
2023-01-18 21:40:11 +0300

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, value) {
+if (value > 0) {
+    let resultString = string;
+    for (i = 0; i < value - 1; i++) {
+        resultString += string;
+    }
+    return resultString; 
+}
 
+else if (value == 0) {
+    return '';
+}
+return 'ERROR';
 };
 
 // Do not edit below this line
