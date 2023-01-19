5cb8333d694e6fdafa90a2908885f90e3a1c0672
Dmitry Shamko <proffery@gmail.com>
2023-01-19 21:44:03 +0300

    04-done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, arg) {
+    for (let j = 0; j < arguments.length; j++) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === arguments[j]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
