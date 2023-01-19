a920098c8a3136c5f147af3dc03d81901beda83b
Dmitry Shamko <proffery@gmail.com>
2023-01-19 22:41:42 +0300

    05-done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    const array = [];
+    let tempArg1 = arg1;
+    let tempArg2 = arg2;
+    let summ = 0;
+    array[0] = arg1;
+    if (arg1 < 0 || arg2 < 0) {
+        return 'ERROR';
+    }
+    
+    if (typeof arg1 != 'number' || typeof arg2 != 'number') {
+        return 'ERROR';
+    }
 
+    if (arg1 > arg2) {
+        tempArg1 = arg2;
+        tempArg2 = arg1;
+        array[0] = arg2;
+    }
+    
+    for (let i = 1; i <= tempArg2 - tempArg1; i++) {
+        array[i] = array[0] + i;
+    }
+    for (let i = 0; i < array.length; i++) {
+        summ += array[i];
+    }
+    return summ;
 };
 
 // Do not edit below this line
