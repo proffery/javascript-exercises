10222d28db3d57977c39950fb1ebf5d9ac69920c
Dmitry Shamko <proffery@gmail.com>
2023-01-19 20:54:58 +0300

    03-done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringToArr = string.split("");
+    let revArr = new Array(stringToArr.length);
+        for (let i = 0; i < stringToArr.length; i++) {
+            revArr[i] = stringToArr[stringToArr.length - i - 1];
+        }
+    return revArr.join("");
 };
 
 // Do not edit below this line
