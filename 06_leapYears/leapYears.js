603f92065131ba84e00fb07f6de9ae1c64edcd60
Dmitry Shamko <proffery@gmail.com>
2023-01-20 20:35:19 +0300

    06-done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeap = false;
+    if (year % 100 == 0 && year % 4 == 0) {
+        if (year % 400 == 0) {
+            isLeap = true;
+        }
+    }
 
+    else if (year % 4 == 0) {
+        isLeap = true;
+    }
+
+    else {
+        isLeap =false;
+    }
+    
+    return isLeap;
 };
 
 // Do not edit below this line
