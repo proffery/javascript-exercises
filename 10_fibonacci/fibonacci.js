0a0ec7279dc737c289a96207f5d7bde30a79bd50
Dmitry Shamko <proffery@gmail.com>
2023-02-01 20:41:31 +0300

    10-done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number >= 2)  {
+        let array = new Array(parseInt(number) + 1);
+        array[0] = 0;
+        array[1] = 1;
+        for (i = 2; i < array.length; i++) {
+            array[i] = array[i - 2] + array[i - 1];
+        }
+        return array[array.length - 1];
+    }
+    else if (number == 1) {
+        return 1;
+    }
+    else {
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
