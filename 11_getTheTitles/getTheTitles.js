fec4c781d445c950d67cfed427b8862852bfa7c1
Dmitry Shamko <proffery@gmail.com>
2023-02-01 21:33:53 +0300

    11-done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titleArr = [books.length];
+    for (let i = 0; i < books.length; i++ ) {
+        titleArr[i] = books[i].title;
+    }
+    return titleArr;
 };
 
 // Do not edit below this line
