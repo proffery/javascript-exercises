ea08753f9ae808720dcb01a328c65709bfa0e613
Dmitry Shamko <proffery@gmail.com>
2023-02-01 23:26:02 +0300

    12-done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(person) {
+    let oldestPersonIndex = 0;
+    let oldestPersonAge = 0;
+    const yearNow = 2023;
+    for (let i = 0; i < person.length; i++) {
+        if (person[i].yearOfDeath === undefined && yearNow - person[i].yearOfBirth > oldestPersonAge) {
+            oldestPersonAge = yearNow - person[i].yearOfBirth;
+            oldestPersonIndex = i;
+        }
+        else if (person[i].yearOfDeath - person[i].yearOfBirth > oldestPersonAge) {
+            oldestPersonAge = person[i].yearOfDeath - person[i].yearOfBirth;
+            oldestPersonIndex = i;
+        }
+    }
+    return person[oldestPersonIndex];
 };
 
 // Do not edit below this line
