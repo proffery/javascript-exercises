424dade1d4e102a39c924aef1355c11ab5b7143a
Dmitry Shamko <proffery@gmail.com>
2023-02-01 23:29:27 +0300

    12-done remove hardcode

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,7 @@
 const findTheOldest = function(person) {
     let oldestPersonIndex = 0;
     let oldestPersonAge = 0;
-    const yearNow = 2023;
+    const yearNow = new Date().getFullYear();
     for (let i = 0; i < person.length; i++) {
         if (person[i].yearOfDeath === undefined && yearNow - person[i].yearOfBirth > oldestPersonAge) {
             oldestPersonAge = yearNow - person[i].yearOfBirth;
